5bcc1490c02f7bcc2f8d51eca0d38dcf755db2b1
psredzinski <patryk.sredzinski@iteo.com>
2021-10-04 10:52:04 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "BoostAI",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "BoostAI",
+            targets: ["BoostAI"]),
+    ],
+    targets: [
+        .target(
+            name: "BoostAI",
+            dependencies: [],
+            path: "BoostAI")
+    ]
+)
